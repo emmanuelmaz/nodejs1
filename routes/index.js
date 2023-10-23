f5db9937ffe6d53bc0f73bf376cc681a9a440940
emmanuelmaz <emmamazzola45@gmail.com>
2023-10-22 23:46:47 -0300

    Interfaz

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const router = Router()
 
 router.get("/", (req, res) => res.render("index", { title: "Primer sitio con node"}))
 
-router.get("/about", (req, res) => res.render("about"))
+router.get("/about", (req, res) => res.render("about", { title: "Página de about"}))
 
-router.get("/contact", (req, res) => res.render("contact"))
+router.get("/contact", (req, res) => res.render("contact", { title: "Página de contact"}))
 
 export default router
